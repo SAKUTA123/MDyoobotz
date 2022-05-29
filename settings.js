600d74841a9a7cac3a0c740a1c982379deb84db0
Profile <91337596+SAKUTA123@users.noreply.github.com>
2022-05-29 12:27:08 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,19 +18,19 @@ global.autoketik = true //status auto mengetik (auto typing)
 global.available = false //status online (online)
 
 // Other
-global.botname = "KATSUBOTZོ"
-global.ownername= "KATSUBOTZ OFC ×፝֟͜×"
+global.botname = "MdBOTZོ"
+global.ownername= "Gilang Md OFC ×፝֟͜×"
 global.myweb ="https://api-alphabot.herokuapp.com/"
-global.youtube = "https://youtube.com/c/ZeeoneOfc"
-global.github = "https://zeeoneofc.github.io/"
-global.ownernomer = "6285290270235"
-global.ownernomerr = "+6285290270235"
+global.youtube = "https://youtube.com/c/animasi_3D"
+global.github = "https://SAKUTA123.github.io/"
+global.ownernomer = "6287760550924"
+global.ownernomerr = "+6287760550924"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6285290270235","6285290270235","6285290270235"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© KATSUBOTZོ' //sticker wm
-global.author = 'Di Buat Oleh KATSU OFC' //sticker wm
+global.owner = ["6287760550924","6287752078797","6287760550924"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© GilangBOTZོ' //sticker wm
+global.author = 'Di Buat Oleh Gilang OFC' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
